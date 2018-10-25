bb7ba06cef2fdb1ac6d5016bdf2dba2fad3daded
danielferreiradf <danielferreiradk@gmail.com>
2018-10-25 10:08:53 -0300

    added mongo backend

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -1,36 +1,33 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { getItems } from '../actions/itemActions';
+import { getItems, deleteItem } from '../actions/itemActions';
 
 import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import uuid from 'uuid';
-
 import PropTypes from 'prop-types';
 
+
 class ShoppingList extends Component {
+
+    componentDidMount() {
+        this.props.getItems();
+    }
+
+    onDeleteClick = (id) => {
+        this.props.deleteItem(id);
+    }
+
     render() {
-        const { item } = this.state;
+        const { items } = this.props.item;
         return (
             <Container>
-                <Button color="dark" style={{ marginBottom: '2rem' }}
-                    onClick={() => {
-                        const name = prompt('Enter Item')
-                        if (name) {
-                            this.setState(state => ({
-                                items: [...state.items, { id: uuid(), name: name }]
-                            }))
-                        }
-                    }}>Add Item</Button>
                 <ListGroup>
                     <TransitionGroup className="shopping-list">
-                        {items.map(({ id, name }) => (
-                            <CSSTransition key={id} timeout={500} classNames="fade">
+                        {items.map(({ _id, name }) => (
+                            <CSSTransition key={_id} timeout={500} classNames="fade">
                                 <ListGroupItem>
                                     <Button className="remove-btn" color="danger" size="sm"
-                                        onClick={() => this.setState(state => ({
-                                            items: this.state.items.filter(item => item.id !== id)
-                                        }))}>&times;
+                                        onClick={this.onDeleteClick.bind(this, _id)}>&times;
                                         </Button>
                                     {name}</ListGroupItem>
                             </CSSTransition>
@@ -51,4 +48,4 @@ const mapStateToProps = (state) => ({
     item: state.item
 })
 
-export default connect(mapStateToProps, { getItems })(ShoppingList);
+export default connect(mapStateToProps, { getItems, deleteItem })(ShoppingList);
--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,20 +1,31 @@
-import uuid from 'uuid';
-import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from '../actions/types';
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING } from '../actions/types';
 
 const initialState = {
-    items: [
-        { id: uuid(), name: 'Eggs' },
-        { id: uuid(), name: 'Milk' },
-        { id: uuid(), name: 'Stake' },
-        { id: uuid(), name: 'Water' },
-    ]
+    items: []
 }
 
 export default function (state = initialState, action) {
     switch (action.type) {
         case GET_ITEMS:
             return {
-                ...state
+                ...state,
+                items: action.payload,
+                loading: false
+            }
+        case ADD_ITEM:
+            return {
+                ...state,
+                items: [action.payload, ...state.items]
+            }
+        case ITEMS_LOADING:
+            return {
+                ...state,
+                loading: true
+            }
+        case DELETE_ITEM:
+            return {
+                ...state,
+                items: state.items.filter(item => item._id !== action.payload)
             }
         default:
             return state
